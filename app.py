6eb6bc4726277b814582b0035950c88f09431b59
huiink <137783839+huiink@users.noreply.github.com>
2025-02-13 14:04:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+from flask import Flask, request, abort
 
 app = Flask(__name__)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-if __name__ == '__main__':
-    app.run(debug=True)
+line_bot_api = LineBotApi(channel_access_token)
+line_handler = WebhookHandler(channel_secret)
+
+# Webhook endpoint
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    
+    try:
+        line_handler.handle(body, signature)
+    except Exception as e:
+        print(e)
+        abort(400)
+    return 'OK'
+
+# 事件處理器
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='你說了: ' + event.message.text)
+    )
+
+if __name__ == "__main__":
+    app.run()
